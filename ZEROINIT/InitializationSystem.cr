c8b5e3dbb6f816468bebe0e71d1b2f8690916dad
Fulgurance <zohran.londais@gmail.com>
2025-01-11 11:42:48 +0000

    Added extra tests

--- ZEROINIT/InitializationSystem.cr
+++ ZEROINIT/InitializationSystem.cr
@@ -7,6 +7,45 @@ module ZEROINIT
         end
 
         def start
+
+	# [ -e /dev/fd ] || ln -snf /proc/self/fd /dev/fd
+	# [ -e /dev/stdin ] || ln -snf /proc/self/fd/0 /dev/stdin
+	# [ -e /dev/stdout ] || ln -snf /proc/self/fd/1 /dev/stdout
+	# [ -e /dev/stderr ] || ln -snf /proc/self/fd/2 /dev/stderr
+	# [ -e /proc/kcore ] && ln -snf /proc/kcore /dev/core
+ #
+	# # Mount required directories as user may not have them in /etc/fstab
+	# for x in \
+	# 	"mqueue /dev/mqueue 1777 ,nodev mqueue" \
+	# 	"devpts /dev/pts 0755 ,gid=5,mode=0620 devpts" \
+	# 	"tmpfs /dev/shm 1777 ,nodev,mode=1777 shm" \
+	# ; do
+	# 	set -- $x
+	# 	grep -Eq "[[:space:]]+$1$" /proc/filesystems || continue
+	# 	mountinfo -q $2 && continue
+ #
+	# 	if [ ! -d $2 ]; then
+	# 		mkdir -m $3 -p $2 >/dev/null 2>&1 || \
+	# 			ewarn "Could not create $2!"
+	# 	fi
+ #
+	# 	if [ -d $2 ]; then
+	# 		ebegin "Mounting $2"
+	# 		if ! fstabinfo --mount $2; then
+	# 			mount -n -t $1 -o noexec,nosuid$4 $5 $2
+	# 		fi
+	# 		eend $?
+	# 	fi
+	# done
+            command = "mknod -m 600 /dev/console c 5 1 && mknod -m 620 /dev/tty1 c 4 1 && mknod -m 666 /dev/tty c 5 0 && mknod -m 666 /dev/null c 1 3 && mknod -m 660 /dev/kmsg c 1 11"
+            process = Process.run(  "#{command}",
+                                        output: Process::Redirect::Inherit,
+                                        error: Process::Redirect::Inherit,
+                                        shell: true)
+
+            rescue
+                puts "Continue anyway"
+
             printInitializationTitle
             printSystemInformation
             printStartingUnitsTitle
